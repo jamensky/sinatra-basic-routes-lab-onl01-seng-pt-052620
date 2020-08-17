e66b98b30f71b9ccc582aa09ae9c2555d1004b88
Jamal Rjoul <jamensky1991@gmail.com>
2020-08-17 18:11:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,16 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get  'name' do 
-     'My name is Jamal'  
+  get  'name' do
+     'My name is Jamal'
   end
 
-  get  'hometown' do 
-     'My hometown is Agadir'  
+  get  'hometown' do
+     'My hometown is Agadir'
   end
 
-  get  'favorite-song' do 
-     'My favorite song is Thriller'  
+  get  'favorite-song' do
+     'My favorite song is Thriller'
   end
-
-
